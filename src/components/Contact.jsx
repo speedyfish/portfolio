767a209fcd060ea2247886ca2e94a3ffcd2cd716
speedyfish <brianlimjj5@gmail.com>
2022-11-28 03:28:01 +0800

    final

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <form method='POST' action="https://getform.io/f/1c38789c-eabf-45c0-8804-e28045f3daba" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>Submit the form below or shoot me an email - brianlimjj5@gmail.com</p>
+                <p className='text-gray-300 py-4'>Submit the form below or shoot me an email: brianlimjj5@gmail.com 📧</p>
             </div>
             <input className='bg-[#ffffff] p-2' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#ffffff]' type="email" placeholder='Email' name='email' />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     const handleClick = () => setNav(!nav)
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-        <div>
+        <div className='mt-5'>
             <img src={Logo} alt="Logo Image" style={{width: " 75px"}} />
         </div>
 
@@ -25,6 +25,11 @@ const Navbar = () => {
             <li>
                 <Link to="about" smooth={true} offset={0} duration={500} >
                     About
+                </Link>
+            </li>
+            <li>
+                <Link to="skills" smooth={true} offset={0} duration={500} >
+                    Skills
                 </Link>
             </li>
             <li>
@@ -62,6 +67,11 @@ const Navbar = () => {
                 <li className='py-6 text-4xl'>
                     <Link to="about" smooth={true} offset={0} duration={500} onClick={handleClick}>
                         About
+                    </Link>
+                </li>
+                <li className='py-6 text-4xl'>
+                    <Link to="skills" smooth={true} offset={50} duration={500} onClick={handleClick}>
+                        Skills
                     </Link>
                 </li>
                 <li className='py-6 text-4xl'>
